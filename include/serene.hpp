adcbc0085fb3a8bcd5c1dd71fa6f8573309b8e4f
June-UwU <williamreji66@gmail.com>
2025-03-23 23:00:08 +0530

    common header

--- include/serene.hpp
+++ include/serene.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <types.hpp>
-
-namespace serene {
-
-}; // serene
+#include "types.hpp"
+#include "free_task.hpp"
+#include "task_base.hpp"
+#include "task_common.hpp"
+#include "relaxed_pool.hpp"
